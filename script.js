1e5804f50b15cb04c0a7ddf2275a8d36fbd15473
Sk1kko <francescolaterza@gmail.com>
2024-12-18 12:39:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,12 @@ const captureBtn = document.getElementById('capture-btn');
 const downloadLink = document.getElementById('download-link');
 const selectedFrame = document.getElementById('selected-frame');
 
+// Settaggio proporzioni 16:9 per il canvas
+const desiredWidth = 1280;
+const desiredHeight = 720;
+canvas.width = desiredWidth;
+canvas.height = desiredHeight;
+
 // Richiesta accesso alla fotocamera
 navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" }, audio: false })
   .then(stream => {
@@ -17,16 +23,12 @@ navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" }, audi
   });
 
 captureBtn.addEventListener('click', () => {
-  // Impostiamo le dimensioni del canvas uguali a quelle del video
-  canvas.width = video.videoWidth;
-  canvas.height = video.videoHeight;
-  
-  // Disegniamo il frame della fotocamera sul canvas
+  // Disegniamo il frame della fotocamera sul canvas in 16:9
   ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
   
   // Disegniamo la cornice sopra l'immagine
   const frameImg = new Image();
-  frameImg.crossOrigin = 'anonymous'; // Impostiamo il crossOrigin
+  frameImg.crossOrigin = 'anonymous';
   frameImg.src = selectedFrame.src;
   frameImg.onload = () => {
     ctx.drawImage(frameImg, 0, 0, canvas.width, canvas.height);
@@ -36,3 +38,4 @@ captureBtn.addEventListener('click', () => {
     downloadLink.style.display = 'inline-block';
   };
 });
+
